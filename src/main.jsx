bd3990da2281de4e06359a7b892932d0d3583b5e
Alifa Ara Heya <alifaaraheya@gmail.com>
2024-10-29 21:40:54 -0400

    added files

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,38 @@
-import { StrictMode } from 'react'
+import * as React from "react";
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import './index.css'
+import App from './App.jsx'
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    // element: <div>Hello world!</div>,
+    element: <Home></Home>,
+    children: [
+
+      {
+        path: '/about',
+        // element: <div>I am in the about page</div>
+        element: <About></About>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      },     
+    ]
   },
-  {
-    path: '/about',
-    element: <div>I am in the about page</div>
-  },
-  {
-    path: '/contact',
-    element: <div>Call me right now!!</div>
-  }
 ]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </React.StrictMode>
 )
